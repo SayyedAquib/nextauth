18aac7ed0675bf75b4221e931dde8db75e8c6ca2
SayyedAquib <sayyed.aquib89@gmail.com>
2025-02-07 21:26:16 +0530

    Add routes for email verification

---
+++ src/app/api/users/verify-email/route.ts
@@ -0,0 +1,41 @@
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import { connect } from "@/dbConfig/dbConfig";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
+connect();
+
+export async function POST(request: NextRequest, response: NextResponse) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log("token: " + token);
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Invalid or expired token" },
+        { status: 401 }
+      );
+    }
+    console.log("user: " + user);
+
+    user.isVarified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Email verified successfully", success: true },
+      { status: 500 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
